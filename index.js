c05784b812aff895368b4d411775e90a4f9e83cb
Vova Kuk <vovakuk7@gmail.com>
2020-01-11 20:15:09 +0200

    Finished

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 function isAdult(age) {
+  if (age <= 0 || typeof age !== 'number') return 'Not valid entry';
   return age >= 21;
 }
 
 function sortArrayUp(arr) {
+  if (!Array.isArray(arr)) return 'Not valid entry';
   return arr.sort((a, b) => a - b);
 }
 
 function isPalindrome(str) {
+  if (typeof str !== 'string') return 'Not valid entry';
   return str.split('').reverse().join('').toLowerCase() === str.toLowerCase();
 }
 
 function isDivisible(n, x, y) {
+  if (typeof n !== 'number' || typeof x !== 'number' || typeof y !== 'number') return 'Not valid entry';
   return n % x === 0 && n % y === 0;
 }
 
 function isOddNumber(number) {
+  if (typeof number !== 'number') return 'Not valid entry';
   return number % 2 !== 0;
 }
 
 function array_diff(a, b) {
+  if (!Array.isArray(a) || !Array.isArray(b)) return 'Not valid entry';
   return a.filter(el => !b.includes(el));
 }
 
 function minSum(arr) {
+  if (!Array.isArray(arr)) return 'Not valid entry';
   let sum = 0;
   arr.sort((a, b) => a - b);
   for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
@@ -32,10 +39,12 @@ function minSum(arr) {
 }
 
 function duplicates(arr) {
+  if (!Array.isArray(arr)) return 'Not valid entry';
   return arr.filter((el, i) => i !== arr.indexOf(el) && i === arr.lastIndexOf(el));
 }
 
 function sumDigits(n) {
+  if (typeof n !== 'number') return 'Not valid entry';
   let sum = 0;
   let a = (n + '').split('');
   for (let i = 0; i < a.length; i++) {
@@ -45,6 +54,7 @@ function sumDigits(n) {
 }
 
 function absentVowel(x) {
+  if (typeof x !== 'string') return 'Not valid entry';
   if (!x.match(/a/)) return 0;
   if (!x.match(/e/)) return 1;
   if (!x.match(/i/)) return 2;
--- test/index.spec.js
+++ test/index.spec.js
@@ -8,6 +8,18 @@ describe('function isAdult', () => {
     //assert.equal(isAdult(21), true);
     expect(isAdult(21)).true;
   });
+
+  it('should function isAdult work correctly', () => {
+    expect(isAdult(11)).false;
+  });
+
+  it('should function isAdult work correctly', () => {
+    expect(isAdult('ten')).eq('Not valid entry');
+  });
+
+  it('should function isAdult work correctly', () => {
+    expect(isAdult(-31)).eq('Not valid entry');
+  });
 });
 
 describe('function sortArrayUp', () => {
@@ -15,11 +27,47 @@ describe('function sortArrayUp', () => {
     //assert.deepEqual(sortArrayUp([4, 1, 7, 2, 0]), [0, 1, 2, 4, 7]);
     expect(sortArrayUp([4, 1, 7, 2, 0])).deep.equal([0, 1, 2, 4, 7]);
   });
+
+  it('should function sortArrayUp work correctly', () => {
+    expect(sortArrayUp([4, 1, 7, 2, 0])).deep.not.equal([4, 1, 7, 4, 0]);
+  });
+
+  it('should function sortArrayUp work correctly', () => {
+    expect(sortArrayUp([])).deep.equal([]);
+  });
+
+  it('should function sortArrayUp work correctly', () => {
+    expect(sortArrayUp()).equal('Not valid entry');
+  });
+
+  it('should function sortArrayUp work correctly', () => {
+    expect(sortArrayUp(456)).equal('Not valid entry');
+  });
 });
 
 describe('function isPalindrome', () => {
   it('should show is it palindrome', () => {
     expect(isPalindrome('madam')).true;
+  });
+
+  it('should show is it palindrome', () => {
+    expect(isPalindrome('MadAm')).true;
+  });
+
+  it('should show is it palindrome', () => {
+    expect(isPalindrome('кит на мор і ром ан тик')).true;
+  });
+
+  it('should show is it palindrome', () => {
+    expect(isPalindrome('MambAm')).false;
+  });
+
+  it('should show is it palindrome', () => {
+    expect(isPalindrome(456)).equal('Not valid entry');
+  });
+
+  it('should show is it palindrome', () => {
+    expect(isPalindrome()).equal('Not valid entry');
   });
 });
 
@@ -27,11 +75,39 @@ describe('function isDivisible', () => {
   it('should show is it number n is divisible by two numbers x AND y', () => {
     expect(isDivisible(10,5,2)).true;
   });
+
+  it('should show is it number n is divisible by two numbers x AND y', () => {
+    expect(isDivisible(15,3,4)).false;
+  });
+
+  it('should function isDivisible work correctly', () => {
+    expect(isDivisible(15,3)).equal('Not valid entry');
+  });
+
+  it('should function isDivisible work correctly', () => {
+    expect(isDivisible(20, 'two','five')).equal('Not valid entry');
+  });
 });
 
 describe('function isOddNumber', () => {
   it('should function isOddNumber work correctly', function () {
     expect(isOddNumber(5)).true;
+  });
+
+  it('should function isOddNumber work correctly', function () {
+    expect(isOddNumber(-11)).true;
+  });
+
+  it('should function isOddNumber work correctly', function () {
+    expect(isOddNumber(10)).false;
+  });
+
+  it('should function isOddNumber work correctly', function () {
+    expect(isOddNumber()).equal('Not valid entry');
+  });
+
+  it('should function isOddNumber work correctly', function () {
+    expect(isOddNumber([7])).equal('Not valid entry');
   });
 });
 
@@ -39,11 +115,35 @@ describe('function array_diff', () => {
   it('should function array_diff work correctly', () => {
     expect(array_diff([4, 1, 7, 2, 0],[1, 2])).deep.equal([4, 7, 0]);
   });
+
+  it('should function array_diff work correctly', () => {
+    expect(array_diff([4, 11, 8, 1, 7, 12, 0],[7, 12, 2])).deep.not.equal([4, 2, 4, 0]);
+  });
+
+  it('should function array_diff work correctly', () => {
+    expect(array_diff([4, 11, 8, 1, 7, 12, 0],7)).equal('Not valid entry');
+  });
+
+  it('should function array_diff work correctly', () => {
+    expect(array_diff([4, 11, 8, 1, 7, 12, 0])).equal('Not valid entry');
+  });
 });
 
 describe('function minSum', () => {
   it('should function minSum work correctly', () => {
     expect(minSum([5, 4, 1, 7, 2, 0])).equal(13);
+  });
+
+  it('should function minSum work correctly', () => {
+    expect(minSum([5, 4, 1, 7, 2, 0])).not.equal(20);
+  });
+
+  it('should function minSum work correctly', () => {
+    expect(minSum()).equal('Not valid entry');
+  });
+
+  it('should function minSum work correctly', () => {
+    expect(minSum(572)).equal('Not valid entry');
   });
 });
 
@@ -51,16 +151,57 @@ describe('function duplicates', () => {
   it('should function duplicates work correctly', () => {
     expect(duplicates([4, 1, 7, 2, 1, 0, 4, 7, 0])).deep.equal([1, 4, 7, 0]);
   });
+
+  it('should function duplicates work correctly', () => {
+    expect(duplicates([4, 1, 8, 2, 1, 0, 1, 7, 0])).deep.not.equal([8, 4, 7, 0]);
+  });
+
+  it('should function duplicates work correctly', () => {
+    expect(duplicates([])).deep.equal([]);
+  });
+
+  it('should function duplicates work correctly', () => {
+    expect(duplicates()).equal('Not valid entry');
+  });
+
+  it('should function duplicates work correctly', () => {
+    expect(duplicates(777)).equal('Not valid entry');
+  });
 });
 
 describe('function sumDigits', () => {
   it('should function sumDigits work correctly', () => {
     expect(sumDigits(578)).equal(20);
   });
+
+  it('should function sumDigits work correctly', () => {
+    expect(sumDigits(1578)).not.equal(20);
+  });
+
+  it('should function sumDigits work correctly', () => {
+    expect(sumDigits('578')).equal('Not valid entry');
+  });
+
+  it('should function sumDigits work correctly', () => {
+    expect(sumDigits()).equal('Not valid entry');
+  });
 });
 
 describe('function absentVowel', () => {
   it('should function absentVowel work correctly', () => {
+    expect(absentVowel('Bb Smith sent us six neatly arranged range bicycles')).not.equal(1);
+  });
+
+  it('should function absentVowel work correctly', () => {
     expect(absentVowel('Bb Smith sent us six neatly arranged range bicycles')).equal(3);
   });
+
+  it('should function absentVowel work correctly', () => {
+    expect(absentVowel()).equal('Not valid entry');
+  });
+
+  it('should function absentVowel work correctly', () => {
+    expect(absentVowel(['Mamba'])).equal('Not valid entry');
+  });
 });
+
